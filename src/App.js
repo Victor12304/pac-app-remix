6d960c75ec403470d879c2c2c068f8842a9ea12b
Juan G De Jesus Torres <jdejesus93@gmail.com>
2019-11-14 11:25:06 -0400

    Minor fixes in jsx files where there was an incorrect use of the "class" keyword in html template.

--- src/App.js
+++ src/App.js
@@ -23,23 +23,23 @@ class App extends Component {
         <div className="App-header">
           <span>Pac App</span>
         </div>
-        <div class="grid-container">
-          <div class="grid-item">
+        <div className="grid-container">
+          <div className="grid-item">
             <Card title="Actividades">
               <Timeline activities={this.state.activities} />
             </Card>
           </div>
-          <div class="grid-item">
+          <div className="grid-item">
             <Card title="Ayuda">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur vero ratione quo, sunt dolores eum. Aut laborum nesciunt quas facilis, facere sint quod assumenda vitae recusandae, porro commodi dignissimos esse!
             </Card>
           </div>
-          <div class="grid-item">
+          <div className="grid-item">
             <Card title="Educacion">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur vero ratione quo, sunt dolores eum. Aut laborum nesciunt quas facilis, facere sint quod assumenda vitae recusandae, porro commodi dignissimos esse!
             </Card>
           </div>
-          <div class="grid-item">
+          <div className="grid-item">
             <Card title="Recursos">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur vero ratione quo, sunt dolores eum. Aut laborum nesciunt quas facilis, facere sint quod assumenda vitae recusandae, porro commodi dignissimos esse!
             </Card>
