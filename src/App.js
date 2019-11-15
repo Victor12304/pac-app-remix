304082e81718691d844b6dcefa6258835e046a11
Juan G De Jesus Torres <jdejesus93@gmail.com>
2019-11-15 16:31:22 -0400

    Applying Modal example with the login form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Timeline from './common/timeline/Timeline';
 import Card from './common/card/Card';
+import Modal from './common/modal/Modal';
+import LoginForm from './common/form/LoginForm';
 
 import './App.css';
 
@@ -8,6 +10,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      show: false,
       activities: [
         { date: "DATE", description: "DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION" },
         { date: "DATE", description: "DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION" },
@@ -16,7 +19,19 @@ class App extends Component {
         { date: "DATE", description: "DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION" }
       ]
     }
+
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
   }
+
+  showModal() {
+    this.setState({ show: true });
+  }
+
+  hideModal() {
+    this.setState({ show: false });
+  }
+
   render() {
     return (
       <div className="App">
@@ -31,6 +46,12 @@ class App extends Component {
           </div>
           <div className="grid-item">
             <Card title="Ayuda">
+              <button type="button" onClick={this.showModal}>
+                open
+            </button>
+              <Modal show={this.state.show} handleClose={this.hideModal}>
+                <LoginForm />
+              </Modal>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur vero ratione quo, sunt dolores eum. Aut laborum nesciunt quas facilis, facere sint quod assumenda vitae recusandae, porro commodi dignissimos esse!
             </Card>
           </div>
